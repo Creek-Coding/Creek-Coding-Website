a4bba0a82c4af64e7c7c9639a595f78e77af018c
jchacko11 <jonathan.j.chacko@gmail.com>
2020-06-02 12:56:21 -0500

    Updated byteCode page

--- src/views/byteCode.vue
+++ src/views/byteCode.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="content is-large">
+        <p>byteCode is our YouTube channel, focused on creating programming language guides and engaging tutorials
+        for beginning and seasoned programmers alike. So far, we have a series for learning Java and another one for
+            Python, with coding livestreams coming in the near future.</p>
 
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/D0UtdNPayWg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </template>
 
