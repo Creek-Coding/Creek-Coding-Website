e25988c240494288802cd62ede008a7c9686c8b5
jchacko11 <jonathan.j.chacko@gmail.com>
2020-06-03 02:35:16 -0500

    Italics philosophy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="home content is-large">
-        <p>Created by students in the Houston area, Creek Coding strives to promote CS literacy through a variety of programs. We focus especially on addressing the growing number of students wanting to learn how to code by sharing our knowledge with those around us.</p>
-        <h1>Our Philosophy: Anyone Can Learn To Code</h1>
-        <p>We believe that anyone should be able to have access to high quality instructional content to learn to code from scratch.
+        <p>Created by students in the Houston area, Creek Coding strives to promote CS literacy through a variety of
+            programs. We focus especially on addressing the growing number of students wanting to learn how to code by
+            sharing our knowledge with those around us.</p>
+        <h1>Our Philosophy: <i>Anyone Can Learn To Code</i></h1>
+        <p>We believe that anyone should be able to have access to high quality instructional content to learn to code
+            from scratch.
             We know that simply reading textbooks isn't the way to learn coding. It's boring and you make slow progress.
-            Aiming to teach through <b>Project Based Learning</b>, we practice and learn new skills by making. At Hack Club,
+            Aiming to teach through <b>Project Based Learning</b>, we practice and learn new skills by making. At Hack
+            Club,
             students are encouraged to build projects every week. Lightbulb Education Code provides high-quality,
             beginner focused
             tutorials on how to make simple but fun projects.</p>
@@ -15,9 +19,14 @@
             <li>It's fun! (Oh and it's a very lucrative career path)</li>
         </ul>
         <p>Of course, coding isn't for everyone. But you won't know until you <b>try</b>. And even if you don't choose
-        it as a career path, you'll still have a valuable skill for the future</p>
+            it as a career path, you'll still have a valuable skill for the future</p>
         <h1>How to get involved</h1>
-        <p>If you're a student at any of the schools we serve (<a href="/schs">SCHS</a> & <a href="/wms">WMS</a>), come to our Hack Club sessions! If you're around our area, talk to your teacher about starting a coding club and we can work with them to get one going! And finaly, if you're not from around here, check out our online learning options. Learn on your own with fun projects at <a href="https://code.lightbulb.education">Lightbulb Education Code</a>, or binge videos from our YouTube channel <a href="https://www.youtube.com/channel/UCHeRnkORi2B3G4kbSyhBtqA">byteCode</a>!</p>
+        <p>If you're a student at any of the schools we serve (<a href="/schs">SCHS</a> & <a href="/wms">WMS</a>), come
+            to our Hack Club sessions! If you're around our area, talk to your teacher about starting a coding club and
+            we can work with them to get one going! And finaly, if you're not from around here, check out our online
+            learning options. Learn on your own with fun projects at <a href="https://code.lightbulb.education">Lightbulb
+                Education Code</a>, or binge videos from our YouTube channel <a
+                    href="https://www.youtube.com/channel/UCHeRnkORi2B3G4kbSyhBtqA">byteCode</a>!</p>
 
     </div>
 </template>
