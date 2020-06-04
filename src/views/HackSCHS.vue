393f09e54aa3a64956bc89b8d0451aaaad1d976e
Adam Nguyen <nguyenadam5@yahoo.com>
2020-06-03 20:58:20 -0500

    changed hackschs page

--- src/views/HackSCHS.vue
+++ src/views/HackSCHS.vue
@@ -1,7 +1,31 @@
 <template>
     <div id="hackschs">
         <div class="content is-large">
-            <HackClub/>
+            <h1>What is Hack Club?</h1>
+            <p>Despite its name, Hack Club is actually a coding club focused on building and creating. We're a part of an international Hack Club community that consists of hundreds of clubs all around the world!
+            </p>
+            <div class="buttons">
+                <a href="https://hackclub.com/" class="button is-primary is-large">The Hack Club Network</a>
+            </div>
+            <div class="columns">
+                <div class="column">
+                    <h1>Learn</h1>
+                    <p>Don't know how to code at all? That's perfectly fine! Come to our classes Thursday afterschool (or watch the livestream)! No experience is required and laptops are provided.
+                    </p>
+                </div>
+                <div class="column">
+                    <h1>Create</h1>
+                    <p>Want to make something cool with coding? Come to our regular Hack Club sessions on Tuesdays afterschool and check out the projects we're working on. You can join some of your peers or start your own project. Whatever you do, you'll have a room full of people ready to help you out. Psst, did we mention free snacks?
+                    </p>
+                </div>
+                <div class="column">
+                    <h1>Teach</h1>
+                    <p>Do you enjoy working with kids and/or want to help others learn how to code? Come help out at the local elementary and middle schools! Currently we are at WMS Tuesdays from 4:00 - 5:00. It's a fun and rewarding experience to see kids engaged in coding and a chance to earn volunteer hours.
+                    </p>
+                </div>
+            </div>
+
+            <!--
             <h2>Projects</h2>
             <div class="project-list">
                 <div class="box">
@@ -15,6 +39,7 @@
                     <a href="" class="button">Make it</a>
                 </div>
             </div>
+            -->
 
         </div>
     </div>
@@ -22,11 +47,9 @@
 </template>
 
 <script>
-    import HackClub from "../components/HackClub";
-
     export default {
         name: "HackSCHS",
-        components: {HackClub}
+        components: {}
     }
 </script>
 
